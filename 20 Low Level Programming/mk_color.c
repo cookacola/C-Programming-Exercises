0064eaac01ddc2a8366ca0d06e25043cdd582212
Charles Chen <117331377+cookacola@users.noreply.github.com>
2023-08-27 14:40:48 -0500

    Reorder RGB

--- 20 Low Level Programming/mk_color.c
+++ 20 Low Level Programming/mk_color.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
-#define MK_COLOR(r,g,b) ((b << 16) | (g << 8) | r)
-#define GET_RED(color) (color << 16 >> 16)
-#define GET_GREEN(color) (8 << color >> 8)
-#define GET_BLUE(color) (color >> 16)
+#define MK_COLOR(r,g,b) ((r << 16) | (g << 8) | b)
+#define GET_BLUE(color) (color) & (0xFF)
+#define GET_GREEN(color) (color >> 8) & (0xFF)
+#define GET_RED(color) (color >> 16) & (0xFF)
 
 /*
 * The OR operator combines all the bits together
 */
 
+/*
+* We use the AND operator to "clear" they bytes we don't want
+*/
+
 int main() {
-    int color = MK_COLOR(180, 255, 255);  // Sets color to white (0xFFFFFF)
-    printf("Color: 0x%X\n", color);
+    int color = MK_COLOR(255, 150, 125);  // Sets color
+    int red = GET_RED(color);
+    int green = GET_GREEN(color);
+    int blue = GET_BLUE(color);
+
+    printf("Color: 0x%X\n Red: 0x%X\n Green: 0x%X\n Blue: 0x%X\n", color, red, green, blue);
     return 0;
 }
