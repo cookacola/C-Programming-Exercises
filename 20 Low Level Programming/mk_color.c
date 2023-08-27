535b8f2a96171ee48a41696b67f4214d929e74eb
Charles Chen <117331377+cookacola@users.noreply.github.com>
2023-08-27 14:21:07 -0500

    MK Color Explanation

--- 20 Low Level Programming/mk_color.c
+++ 20 Low Level Programming/mk_color.c
@@ -2,6 +2,10 @@
 
 #define MK_COLOR(r,g,b) ((b << 16) | (g << 8) | r)
 
+/*
+* The OR operator combines all the bits together
+*/
+
 int main() {
     int color = MK_COLOR(180, 255, 255);  // Sets color to white (0xFFFFFF)
     printf("Color: 0x%X\n", color);
