7df9d51715d5de3ce8bb74d783d125534c001e12
Charles Chen <117331377+cookacola@users.noreply.github.com>
2023-08-27 13:13:59 -0500

    XOR Encryption

---
+++ 20 Low Level Programming/xor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define KEY '&'
+
+main() {
+    int orig_char, new_char;
+
+    while((orig_char = getchar()) != EOF) {
+        new_char = orig_char ^ KEY; /* Apply encryption */
+
+        /* Check if control character */
+        if(iscntrl(orig_char) || iscntrl(new_char)) {
+            putchar(orig_char);
+        } else
+            putchar(new_char);
+    }
+    return 0;
+}
