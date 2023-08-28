3474e1263b5c8c1bc9609c97f79a00c62a5923ff
Charles Chen <117331377+cookacola@users.noreply.github.com>
2023-08-27 21:49:09 -0500

    Float field union

--- 20 Low Level Programming/float_bit_field.c
+++ 20 Low Level Programming/float_bit_field.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
-struct float_bit_field {
-        unsigned int sign : 1;
-        unsigned int exponent : 8;
+union float_union {
+    float i;
+    struct float_bit_field {
         unsigned int fraction : 23;
-} float1 = {1, 5, 23};
+        unsigned int exponent : 8;
+        unsigned int sign : 1;        
+    } fbf;
+} fu;
 
 int main() {
-    printf("Sign: %d, Exponent: %d, Fraction: %d", float1.sign, float1.exponent, float1.fraction);
+    fu.fbf.sign = 1;
+    fu.fbf.exponent = 128;
+    fu.fbf.fraction = 0;
+    printf("Float: %f\n", fu.i);
+    printf("Sign: %d, Exponent: %d, Fraction: %d", fu.fbf.sign, fu.fbf.exponent, fu.fbf.fraction);
     return 0;
 }
